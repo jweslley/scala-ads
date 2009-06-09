bd9699a5138cee7fae61adcdb74a6bee1d62c8a2
Jonhnny Weslley <jonhnnyweslley@gmail.com>
2009-06-08 22:57:06 -0300

    Simple Bloom filter implementation, it needs test!

--- src/main/scala/scalaads/BloomFilter.scala
+++ src/main/scala/scalaads/BloomFilter.scala
@@ -4,17 +4,17 @@ trait BloomFilter[A] extends (A => Boolean) {
 
   def +(e: A): BloomFilter[A]
 
-	/**
-	 * False indicates that 'e' was definitely not added to this Bloom Filter, 
-	 * true indicates that it probably was. The probability can be estimated
-	 * using the falsePositiveRate() method.
-	 */
+  /**
+  * False indicates that 'e' was definitely not added to this Bloom Filter, 
+  * true indicates that it probably was. The probability can be estimated
+  * using the falsePositiveRate() method.
+  */
   def contains(e: A): Boolean
 
   def apply(e: A) = contains(e)
 
-	/**
-	 * The number of bits in the bit array,
+  /**
+   * The number of bits in the bit array,
    * often called 'm' in the context of bloom filters.
    */
   def size: Int
@@ -23,15 +23,15 @@ trait BloomFilter[A] extends (A => Boolean) {
    * A list of hash functions that are used to set and test the bits.
    * These hash functions map each added item to a random number
    * uniform over the range {1..size} for mathematical convenience.
-	 * The number of hash functions is often called 'k' in the context of bloom filters.
+   * The number of hash functions is often called 'k' in the context of bloom filters.
    */
   def hashes: List[A => Int]
 
-	/**
-	 * Calculates the approximate probability of the contains() method returning
-	 * true for an object that had not previously been inserted into the bloom
-	 * filter. This is known as the "false positive probability".
-	 */
+   /**
+   * Calculates the approximate probability of the contains() method returning
+   * true for an object that had not previously been inserted into the bloom
+   * filter. This is known as the "false positive probability".
+   */
   def falsePositiveRate: Double
 
 }
