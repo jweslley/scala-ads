6c18029c8b58e9cdd09039fa2de8c8a5b90af857
Jonhnny Weslley <jonhnnyweslley@gmail.com>
2009-12-13 11:55:51 -0300

    Migration from Maven to sbt

---
+++ project/build/ScalaADSProject.scala
@@ -0,0 +1,10 @@
+import sbt._
+
+/**
+ * Algorithms and Data Structures implementations for Scala
+ */
+class Project(info: ProjectInfo) extends DefaultProject(info) {
+
+  val specs = "org.specs" % "specs" % "1.4.3"
+
+}
